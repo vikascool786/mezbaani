1b877641d72db378afb169113cbb49e0381ea4b2
vikas wankhede <vikasw@marketamerica.com>
2025-12-30 18:32:08 +0530

    all

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { User, Role, Restaurant } = require("../models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -111,7 +110,7 @@ exports.login = async (req, res) => {
         phone: user.phone,
         roleName: user.role.roleName,
       },
-      'sesssecret',
+      "sesssecret",
       { expiresIn: "7d" }
     );
 
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const path = require("path");
-
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
-
+// index.js
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { sequelize } = require('./models');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.APP_PORT || 5003;
+const PORT = process.env.PORT || 5003;
 
 app.use(cors());
 app.use(express.json());
