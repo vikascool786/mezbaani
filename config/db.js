58dcc29e0e6058e203ed02e5498269b74b68efe8
root <root@srv820954.hstgr.cloudvitsolutions24x7.com>
2025-12-30 11:47:44 +0000

    live updated

--- config/db.js
+++ config/db.js
@@ -1,11 +1,18 @@
+
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  dialect: 'mysql',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  'mezbaani',
+  'admin',
+  'Poonam#@#1988',
+  {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { sequelize } = require('./models');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
+app.use('/mezbaani/api', routes);
 
-app.get('/', (req, res) => {
+app.get('/mezbaani/', (req, res) => {
   res.send('🚀 Mezbaani API is running');
 });
 
